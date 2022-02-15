9d91f08de905bac21b0e6adcd1b457663e5faaef
NardiniA <techteacher.pershore@gmail.com>
2022-02-15 15:45:16 +0000

    Updated contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -25,48 +25,60 @@ $(function() {
       fetch("https://api.emailjs.com/api/v1.0/email/send", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          service_id: 'service_562kegs',
-          template_id: 'template_grbq0qg',
-          user_id: 'user_pwnrSymGTUtZ4MVyEqwmT',
+          service_id: "service_4gn38jh",
+          template_id: "template_grbq0qg",
+          user_id: "user_pwnrSymGTUtZ4MVyEqwmT",
           template_params: {
             from_name: name,
             service: service,
             message: message,
             address: address,
             reply_to: email,
-            phone_number: phone
-          }
-        })
+            phone_number: phone,
+          },
+        }),
       })
-        .then(res => res.text())
-        .then(data => {
+        .then((res) => res.text())
+        .then((data) => {
           console.log(data);
           if (data === "OK") {
             // Success message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            $("#success").html("<div class='alert alert-success'>");
+            $("#success > .alert-success")
+              .html(
+                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+              )
               .append("</button>");
-            $('#success > .alert-success')
-              .append("<strong>Your message has been sent. </strong>");
-            $('#success > .alert-success')
-              .append('</div>');
+            $("#success > .alert-success").append(
+              "<strong>Your message has been sent. </strong>"
+            );
+            $("#success > .alert-success").append("</div>");
             //clear all fields
-            $('#contactForm').trigger("reset");
+            $("#contactForm").trigger("reset");
           } else {
             // Fail message
-            $('#success').html("<div class='alert alert-danger'>");
-            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            $("#success").html("<div class='alert alert-danger'>");
+            $("#success > .alert-danger")
+              .html(
+                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+              )
               .append("</button>");
-            $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that my mail server is not responding. Please try again later!"));
-            $('#success > .alert-danger').append('</div>');
+            $("#success > .alert-danger").append(
+              $("<strong>").text(
+                "Sorry " +
+                  name +
+                  ", it seems that my mail server is not responding. Please try again later!"
+              )
+            );
+            $("#success > .alert-danger").append("</div>");
             //clear all fields
-            $('#contactForm').trigger("reset");
+            $("#contactForm").trigger("reset");
           }
         })
-        .catch(e => alert("Error... " + e))
+        .catch((e) => alert("Error... " + e));
     },
     filter: function() {
       return $(this).is(":visible");
